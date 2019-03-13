da35e9df850d42a20edda1f168996f16cc474c42
“manersurekalpesh” <kalpesh.manersure@onmobile.com>
2019-03-13 15:05:15 +0530

    First

---
+++ Demo1234/src/mypack/Maine.java
@@ -0,0 +1,18 @@
+package mypack;
+
+
+/**
+ * Main class for calling the message class
+ *
+ */
+public class Maine {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Message m=new Message();
+		m.disp();
+	}
+
+}
---
+++ Demo1234/src/mypack/Message.java
@@ -0,0 +1,9 @@
+package mypack;
+
+public class Message {
+	void disp()
+	{
+		System.out.println("Displaying the message");
+	}
+
+}
